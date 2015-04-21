5c1aee10db496ff907736d0c9c4a02b39fcbc76d
Catherine <yacatherine19@gmail.com>
2015-04-21 17:53:52 +0800

    多线程断点续传 完成

---
+++ src/com/example/download/StreamTools.java
@@ -0,0 +1,25 @@
+package com.example.download;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+public class StreamTools {
+	public static final String TAG = "StreamTools";
+
+	/**
+	 * 把一个inputstream里面的内容转化成一个byte[]
+	 */
+	public static byte[] getBytes(InputStream is) throws Exception {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while ((len = is.read(buffer)) != -1) {
+			bos.write(buffer, 0, len);
+		}
+		is.close();
+		bos.flush();
+		byte[] result = bos.toByteArray();
+		// System.out.println(new String(result));
+		return result;
+	}
+}
--- src/com/example/download/TestDownload.java
+++ src/com/example/download/TestDownload.java
@@ -1,5 +1,7 @@
 package com.example.download;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
@@ -32,7 +34,7 @@ public class TestDownload {
 			 * "rws" 打开以便读取和写入，对于 "rw"，还要求对文件的内容或元数据的每个更新都同步写入到底层存储设备。<br>
 			 * "rwd" 打开以便读取和写入，对于 "rw"，还要求对文件内容的每个更新都同步写入到底层存储设备。
 			 */
-			//产生一个跟服务器文件大小一致的空文件
+			// 产生一个跟服务器文件大小一致的空文件
 			RandomAccessFile file = new RandomAccessFile(getFileName(path),
 					"rwd");
 			// 1.在本地创建一个文件 文件大小要跟服务器文件的大小一致
@@ -93,9 +95,25 @@ class DownloadTask extends Thread {
 	public void run() {
 
 		try {
+			// 创建一个文件对象，记录当前某个文件的下载位置
+			File positionFile = new File(threadId + ".txt");
+
 			URL url = new URL(filePath);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			//
+			System.out.println("线程" + threadId + "正在下载" + "  开始位置"
+					+ startPosition + "  结束位置" + endPosition);
+
+			if (positionFile.exists()) {
+				FileInputStream fis = new FileInputStream(positionFile);
+				byte[] result = StreamTools.getBytes(fis);
+				// byte[]转int，先转strint,才能转int
+				int newStartPosition = Integer.parseInt(new String(result));
+
+				if (newStartPosition > startPosition) {
+					startPosition = newStartPosition;
+				}
+			}
+
 			conn.setRequestProperty("Range", "bytes=" + startPosition + "-"
 					+ endPosition);
 			conn.setConnectTimeout(5000);
@@ -112,15 +130,37 @@ class DownloadTask extends Thread {
 			// 文件长度
 			// 当length = -1代表文件读完了
 			int length = 0;
+
+			// 当前读到服务器数据的位置，同时这个值已经存储的文件的位置
+			int currentPosition = startPosition;
+
 			while ((length = is.read(buffer)) != -1) {
 				file.write(buffer, 0, length);
+
+				// 需要把currentPosition信息持久化到存储设备
+				currentPosition += length;
+				String position = currentPosition + "";
+
+				FileOutputStream fos = new FileOutputStream(positionFile);
+				fos.write(position.getBytes());
+				fos.flush();
+				fos.close();
+
 			}
+
 			file.close();
 			System.out.println("线程" + threadId + "下载完毕");
+
+			// 当线程下载完毕后，把currentPosition的文件删除
+			if (positionFile.exists()) {
+				positionFile.delete();
+			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		super.run();
 	}
 
